dbadfe3a95620770fc030465513a563b4988532e
hamzas121 <Hamza@Hamzas-MacBook-Pro.local>
2017-02-15 10:12:28 +0000

    updated javascript

--- javascript.js
+++ javascript.js
@@ -36,7 +36,20 @@ $(document).ready(function(){
 
 
 
-
+function move() {
+  var elem = document.getElementById("myBar");   
+  var width = 10;
+  var id = setInterval(frame, 10);
+  function frame() {
+    if (width >= 100) {
+      clearInterval(id);
+    } else {
+      width++; 
+      elem.style.width = width + '%'; 
+      document.getElementById("label").innerHTML = width * 1  + '%';
+    }
+  }
+}
 
 
   
