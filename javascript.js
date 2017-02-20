f4c127269f046856ed6097831bcf1095ff7431c7
hamzas121 <Hamza@Hamzas-MacBook-Pro.local>
2017-02-20 13:07:40 +0000

    main changes
    ful website with updated images and index page changed

--- javascript.js
+++ javascript.js
@@ -34,22 +34,13 @@ $(document).ready(function(){
   });
 });
 
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("image1").on('click', function(event) {
 
-
-function move() {
-  var elem = document.getElementById("myBar");   
-  var width = 10;
-  var id = setInterval(frame, 10);
-  function frame() {
-    if (width >= 100) {
-      clearInterval(id);
-    } else {
-      width++; 
-      elem.style.width = width + '%'; 
-      document.getElementById("label").innerHTML = width * 1  + '%';
-    }
-  }
-}
+ // End if
+  });
+});
 
 
   
